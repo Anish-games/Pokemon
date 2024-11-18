07aa5caba0a89c4eef028d15a35c0675af3450b9
MiniNija <anishkute12@gmail.com>
2024-11-18 19:35:57 +0530

    Revert "Initialization of project"
    This reverts commit 54fe486829a5635c4ff12ad2137edf127849c411.

---
+++ Pokemon.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string PlayerName;
+    cout << "Enter your name: ";
+    cin >> PlayerName;
+    cout << "Welcome, " << PlayerName << "!" << endl;
+    cout << "🌟 Your Pokemon adventure begins now! 🌟" << endl;
+    cout << "In the world of Pokemon, you'll encounter many wild creatures, battle fierce opponents, and capture your very own Pokemon team." << endl;
+    cout << "Remember, every challenge is an opportunity to become a better trainer. Trust in your Pokemon, believe in yourself, and embark on this exciting journey!" << endl;
+    cout << "Let's get started, " << PlayerName << ". The world of Pokémon awaits you!" << endl;
+    cout << "✨ Good luck, and may you become the ultimate Pokemon Master! ✨" << endl;
+
+}
